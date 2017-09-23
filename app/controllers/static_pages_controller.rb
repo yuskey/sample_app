54a8e5fb41b2ffc975d11fe192059c47524f7d7c
yuskey <heredia.brian@gmail.com>
2017-09-23 17:42:56 +0000

    commiting other files

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  #the view (html webpage) associated with the home function is found in /app/views/home.html.erb
+  def home
+    
+  end
+
+  #the view (html webpage) associated with the help function is found in /app/views/help.html.erb
+  def help
+  end
+end

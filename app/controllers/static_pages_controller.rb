39b868b70bcbe5da8facbd46862cfa43215e82f7
yuskey <heredia.brian@gmail.com>
2017-09-24 17:38:28 +0000

    Added about page and tests for getting URLS for home help and about

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,12 @@
 class StaticPagesController < ApplicationController
   #the view (html webpage) associated with the home function is found in /app/views/home.html.erb
   def home
-    
   end
 
   #the view (html webpage) associated with the help function is found in /app/views/help.html.erb
   def help
+  end
+  
+  def about
   end
 end

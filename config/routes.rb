228e027b75649ec258b627a246dcdb58a30663b2
yuskey <heredia.brian@gmail.com>
2017-10-21 20:55:10 +0000

    Finished up to 5.4

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   #states that when you go to the website www.url.com/static_pages/home it will execute 
   #whatever is in the home function in the controller, same for the others
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
 end
